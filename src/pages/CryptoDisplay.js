6fbe85d15218a6b5b9b4d7ed00e11be9e4948ad3
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-05-17 11:14:00 -0400

    added data to CryptoDisplay. About to start on chart.js

--- src/pages/CryptoDisplay.js
+++ src/pages/CryptoDisplay.js
@@ -33,42 +33,42 @@ export default function DisplayPage(props) {
     return <h1>loading ...</h1>
   } else if (cryptoData.description.en === undefined) {
     return <h1>loading ...</h1>
-  } 
+  }
 
 
-
-  const remover = /(<([^>]+)>)/ig;
+  const regex = /(<([^>]+)>)/ig;
 
     return (
         <div>
             <div>
-                {cryptoData.name}
+                <h1>{cryptoData.name}</h1>
+                <img src={cryptoData.image.small}></img>
+                <h2>{cryptoData.symbol.toUpperCase()}</h2>
             </div>
             <div>
-                {cryptoData.description.en.replace(remover, '')}
-
-                {/* {description.map((data) => {
-                    console.log(data)
-                })} */}
-                
-                {/* {description.map((crypto) => {
-                    console.log(crypto)
-                    return crypto.map((des) => {
-                        console.log(des)
-                        return (
-                          <div>1</div>
-                        )
-                      })
-                })} */}
-
-                {/* {description.map((crypto) => {
-                    console.log(JSON.stringify(crypto, null, 4));
-                    return (
-                        <div>1</div>
-                    )})} */}
-
-
-
+                <h5><a href={cryptoData.links.homepage}>{cryptoData.links.homepage}</a></h5>
+                <h5><a href={cryptoData.links.official_forum_url}>{cryptoData.links.official_forum_url}</a></h5>
+            </div>
+            <div>
+                <h3>Current price: ${cryptoData.market_data.current_price.usd}</h3>
+                <h3>Market Cap rank: {cryptoData.market_cap_rank}</h3>
+                <h3>Market Cap: ${cryptoData.market_data.market_cap.usd}</h3>
+                <h3>All time high: ${cryptoData.market_data.ath.usd}</h3>
+                <h3>Total volume: ${cryptoData.market_data.total_volume.usd}</h3>
+                <h3>24h high: ${cryptoData.market_data.high_24h.usd}</h3>
+                <h3>24h low: ${cryptoData.market_data.low_24h.usd}</h3>
+                <h3>24h price change: ${cryptoData.market_data.price_change_24h_in_currency.usd}</h3>
+                <h3>24h percent change: {cryptoData.market_data.price_change_percentage_24h}%</h3>
+                <h3>7 day percent change: {cryptoData.market_data.price_change_percentage_7d}%</h3>
+                <h3>14 day percent change: {cryptoData.market_data.price_change_percentage_14d}%</h3>
+                <h3>30 day percent change: {cryptoData.market_data.price_change_percentage_30d}%</h3>
+                <h3>1 year percent change: {cryptoData.market_data.price_change_percentage_1y}%</h3>
+                <h3>Circulating Supply: {cryptoData.market_data.circulating_supply} coins</h3>
+            </div>
+            <div>
+                <p>
+                    {cryptoData.description.en.replace(regex, '')}
+                </p>
             </div>
         </div>
     )
